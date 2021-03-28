a0426cd187ba765327186f5add4e9ce2e055bea0
AyeAreEm <ag.ayeareem@gmail.com>
2021-03-28 22:35:42 +0800

    fixed fetch request url again

--- views/script.js
+++ views/script.js
@@ -22,7 +22,7 @@ async function load(category) {
     let newCategories = [];
     let loadBtn = document.getElementById("loadBtn");
     // get json data for all movies in a certain category
-    let res = await fetch(`http://ayemovies.herokuapp.com/${category}`);
+    let res = await fetch(`https://ayemovies.herokuapp.com/${category}`);
     let result = await res.json();
 
     // create a div to surround the links
